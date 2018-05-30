caf3b0dc7b7d1d87143e3dbd83dd2a03b13d79d2
Diddakk <diegoriverariera@gmail.com>
2018-05-30 23:25:26 +0200

    get url on tab

--- PageFontStyle/popup/popup.js
+++ PageFontStyle/popup/popup.js
@@ -7,6 +7,12 @@ $(function () {
     $("#btnChange").click(function () {
         chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {todo: "changeColor", clickedColor: color});
-        });  
+        });
+        chrome.tabs.getSelected(null, function (tab) {
+            var url = new URL(tab.url)
+            var domain = url.hostname
+            // `domain` now has a value like 'example.com'
+            console.log(domain);
+          })  
     });
 });
